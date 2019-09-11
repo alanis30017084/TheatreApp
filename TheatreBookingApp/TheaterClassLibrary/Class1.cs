a7b336313ffa406d8a62bb657314680e49e4a571
Alanis <alanis.ck@gmail.com>
2019-09-12 10:39:50 +1200

    Initial commit

---
+++ TheatreBookingApp/TheaterClassLibrary/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheaterClassLibrary
+{
+    public class Theater
+    {
+        public static Seat[,] theater1 = new Seat[10, 10];
+        public static Seat[,] theater2 = new Seat[10, 10];
+        public static Seat[,] theater3 = new Seat[10, 10];
+    }
+}
---
+++ TheatreBookingApp/TheaterClassLibrary/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheaterClassLibrary
+{
+    public class Seat
+    {
+        public bool Booked { get; set; } = false;
+        public string AdultOrChild { get; set; }
+
+        public Seat(bool booked, string adultOrChild)
+        {
+            Booked = booked;
+            AdultOrChild = adultOrChild;
+        }
+    }
+}
